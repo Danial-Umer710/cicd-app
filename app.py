5efbd0b455516187948e201660fa9086909e4fc4
Danial Umer <danialumer710@gmail.com>
2026-03-24 16:09:11 +0100

    Added the build message feature

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask, render_template
-from datetime import datetime, timedelta
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
+def get_version():
+    return os.getenv("APP_VERSION", "unknown")
+
+def get_time():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def get_message():
+    return os.getenv("BUILD_MESSAGE", "First deployment 🚀")
+
 @app.route("/")
 def home():
-    current_time = datetime.utcnow() + timedelta(hours=1)
-    current_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    return render_template("index.html", time=current_time)
+    return render_template(
+        "index.html",
+        version=get_version(),
+        time=get_time(),
+        message=get_message()
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
