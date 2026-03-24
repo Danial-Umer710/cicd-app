7bfa5a29b401375fe1c10dcbd16bfdbb5829d716
Danial Umer <danialumer710@gmail.com>
2026-03-24 14:48:24 +0100

    fixed timezone display

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    current_time = datetime.utcnow() + timedelta(hours=1)
+    current_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    return render_template("index.html", time=current_time)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
